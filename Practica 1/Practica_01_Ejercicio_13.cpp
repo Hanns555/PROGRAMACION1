3170bae1c8188abf5da5b33bd82ddb27d2a2846b
Hanns555 <hanns.silva@ucb.edu.bo>
2025-08-27 22:35:28 -0400

    Add files via upload

---
+++ Practica 1/Practica_01_Ejercicio_13.cpp
@@ -0,0 +1,17 @@
+// Materia: Programación I, Paralelo 4 
+// Autor: Willy Edwin Tenorio Palza 
+// Carnet: 8357097 L.P. 
+// Carrera del estudiante: Ingeniería Biomedica 
+// Fecha creación: 24/08/2025 
+// Número de ejercicio: 13
+#include <iostream>
+using namespace std;
+
+int main() {
+   int numero, suma; 
+   cout<< "Ingrese un numero: ";
+   cin>> numero;
+   suma=(numero*(numero+1))/2;
+   cout<<"La suma es"<<suma;
+return 0;
+}
