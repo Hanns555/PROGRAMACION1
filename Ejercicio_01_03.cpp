9ad5fb0bdaba3fa2aa432ea2726f5e886d71ed76
Hanns555 <hanns.silva@ucb.edu.bo>
2025-08-29 22:54:37 -0400

    Update and rename Practica 1/Practica_01_Ejercicio_03.cpp to Ejercicio_01_03.cpp

--- Ejercicio_01_03.cpp
+++ Ejercicio_01_03.cpp
@@ -1,19 +1,22 @@
 // Materia: Programación I, Paralelo 4 
-// Autor: Willy Edwin Tenorio Palza 
+// Autor: Hanns Michael Silva Pardo 
 // Carnet: 8357097 L.P. 
 // Carrera del estudiante: Ingeniería Biomedica 
 // Fecha creación: 24/08/2025 
-// Número de ejercicio: 2
+// Número de ejercicio: 3
 #include <iostream>
 using namespace std;
 
 int main() {
-    float b, h, area; //De esta forma se debine las variables b,h,area como flotantes
+    //De esta forma se debine las variables b,h,area como flotantes
+    float base, altura, area; 
     cout << "Ingrese la base de tu triangulo: ";
-    cin >> b;
+    cin >> base;
     cout << "Ingrese la altura de tu triangulo: ";
-    cin >> h;
-    area = (b * h) / 2; //Definimos area con la formula de calcular area de un triangulo
+    cin >> altura;
+    //Definimos area con la formula de calcular area de un triangulo
+    area = (base * altura) / 2; 
     cout << "El area de tu triangulo es: " << area << endl;
     return 0;
 }
+
