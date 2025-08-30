7df5d57f5e98e98091f5becbb7ac6c5f1d58a335
Hanns555 <hanns.silva@ucb.edu.bo>
2025-08-29 21:30:16 -0400

    Update and rename Practica_01_Ejercicio_01.cpp to Ejercicio_01_01.cpp

--- Practica 1/Ejercicio_01_01.cpp
+++ Practica 1/Ejercicio_01_01.cpp
@@ -1,5 +1,5 @@
 // Materia: Programación I, Paralelo 4 
-// Autor: Willy Edwin Tenorio Palza 
+// Autor: Hanns Michael Silva Pardo
 // Carnet: 8357097 L.P. 
 // Carrera del estudiante: Ingeniería Biomedica 
 // Fecha creación: 24/08/2025 
@@ -8,9 +8,11 @@
 using namespace std;
 
 int main() {
+    // Los endl hacen que al correr el programa los mensajes salgan uno debajo del otro y no todo en una sola linea
     cout << "Hola, soy un estudainete de Programacion." <<endl;
     cout << "Mi nombre es Hanns Michael Silva Pardo." << endl;
     cout << "Soy de la carrera de Ingenieria Biomedica." << endl;
     return 0;
 }
+
 
